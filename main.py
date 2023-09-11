e17f5dd6b9252e93971ac3b082c7e435a9fd9077
astrapninja <astrapninja@gmail.com>
2023-09-11 02:24:38 +0100

    Version 0,2

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import pygame, sys, time
+import pygame, sys, time, random
 pygame.init()
-
 
 screenX, screenY = 640,360
 screen  = pygame.display.set_mode((screenX, screenY))
@@ -11,31 +10,39 @@ class Player(pygame.sprite.Sprite):
         super().__init__()
         self.image = pygame.image.load("sprites/Player/PlayerRocket.png").convert_alpha()
         self.rect = self.image.get_rect(midbottom = (320, 330))
-        self.bullet_group = pygame.sprite.Group()
-        self.bullet_timer = 1
-
+        self.bullet_last = 0
 
     def player_input(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a] or keys[pygame.K_LEFT]:
             self.rect.x -= 16
             if keys[pygame.K_SPACE] or keys[pygame.K_KP_ENTER] or keys[pygame.K_RETURN]:
-                bullet = Bullet(self.rect.x+24, self.rect.y)
-                self.bullet_group.add(bullet)
+                self.bullet_load(24)
+        
         if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
             self.rect.x += 16
             if keys[pygame.K_SPACE] or keys[pygame.K_KP_ENTER] or keys[pygame.K_RETURN]:
-                bullet = Bullet(self.rect.x-16, self.rect.y)
-                self.bullet_group.add(bullet)
+                 self.bullet_load(-16)
+        
         if (keys[pygame.K_SPACE] or keys[pygame.K_KP_ENTER] or keys[pygame.K_RETURN]) and not (keys[pygame.K_a] or keys[pygame.K_LEFT] or keys[pygame.K_d] or keys[pygame.K_RIGHT]):
-            
-            bullet = Bullet(self.rect.x+8, self.rect.y)
-            self.bullet_group.add(bullet)
+            self.bullet_load(8)
+
+    def bullet_load(self, value):
+            timer = pygame.time.get_ticks()
+            if timer-self.bullet_last >= 256:
+                bullet = Bullet(self.rect.x+value, self.rect.y)
+                bullet_group.add(bullet)
+                self.bullet_last = pygame.time.get_ticks()
+
+    def boundary(self):
+        if self.rect.x < 0:
+            self.rect.x = 0
+        if self.rect.x > screenX-16:
+             self.rect.x = screenX-16
 
     def update(self):
          self.player_input()
-         self.bullet_group.draw(screen)
-         self.bullet_group.update()
+         self.boundary()
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self,X,Y):
@@ -43,16 +50,53 @@ class Bullet(pygame.sprite.Sprite):
         self.image = pygame.image.load("sprites/Player/Bullet.png").convert_alpha()
         self.rect = self.image.get_rect(center = (X, Y))
     
+    def destroy(self):
+        bullet_group.remove(self)
+    
+    def bullet_hit(self):
+        got_hit = pygame.sprite.spritecollide(self, alien_group, True)
+        if got_hit:
+            got_hit[0].deplete_health()
+            self.destroy()
+    
+    def boundary(self):
+        if self.rect.y < 0:
+            self.destroy()
+
     def update(self):
-         self.rect.y -= 8
+        self.boundary()
+        self.bullet_hit()
+        self.rect.y -= 8
 
 class Alien(pygame.sprite.Sprite):
-    def __init__(self, place):
-        x = 32
-        x = (place*64) + x 
+    def __init__(self, row, col, cols):
         super().__init__()
+        row = (row*64) + 64
+        col = (col*screenX/cols) + (screenX/cols)/2
         self.image = pygame.image.load("sprites/alien/alien1.png").convert_alpha()
-        self.rect = self.image.get_rect(midbottom = (x,100))
+        self.rect = self.image.get_rect(midbottom = (col, row))
+        self.health = 2
+        self.cooldown = 1600
+        self.last_tick = 1601
+        self.move = random.randint(1,6)
+        self.roll = True
+    
+    def kill(self):
+        if self.health <= 0:
+            alien_group.remove(self)
+
+    def deplete_health(self):
+        self.health -= 1
+        self.kill()
+    
+    def pace(self):
+        timer = pygame.time.get_ticks()
+        if timer-self.last_tick >= self.cooldown:
+            self.rect.y += 8
+            self.last_tick = pygame.time.get_ticks()
+            
+    def update(self):
+        self.pace()
 
 game_active = True
 
@@ -61,13 +105,18 @@ player_group = pygame.sprite.GroupSingle()
 player = Player()
 player_group.add(player)
 
+#bullet
+bullet_group = pygame.sprite.Group()
+
 #alien
 alien_group = pygame.sprite.Group()
-for place in range(10):
-            alien = Alien(place)
-            alien_group.add(alien)
-
-
+rows = 3
+cols = 10
+for row in range(rows):
+    for col in range(cols):
+        alien = Alien(row, col, cols)
+        alien_group.add(alien)
+    cols -= 1
 
 while True:
 
@@ -81,6 +130,9 @@ while True:
         player_group.draw(screen)
         player.update()
         alien_group.draw(screen)
+        alien_group.update()
+        bullet_group.draw(screen)
+        bullet_group.update()
 
     pygame.display.update()
     clock.tick(24)
